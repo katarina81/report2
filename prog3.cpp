62409e141a8035b145afd8de62eb57d0b9a34a56
katarina81 <kat@sings.jp>
2018-12-29 03:02:44 +0900

    ex

--- prog3.cpp
+++ prog3.cpp
@@ -13,14 +13,13 @@ class addressbook{
   person friends[100];
   person address[100];
   person no_one;
-  no_one.name = "";
-  return no_one;
-  int numberOfFriends;
 
 public:
   addressbook();
   void listFriends();
   void addFriend(string name, string address);
+  int numberOfFriends;
+  void find(string query);
 };
 
 // constructor
@@ -41,11 +40,24 @@ void addressbook::listFriends(){
   }
 }
 
-int main()
-{
+void addressbook::find(string query){
+  for(int i = 0; i < numberOfFriends; i++){
+    if(query == friends[i].name){
+      cout << friends[i].name << ":";
+      cout << address[i].address << "\n";
+    }else{
+      no_one.name = "";
+      cout << no_one.name << "\n";
+    }
+  }
+}
+
+int main(){
   addressbook abook;
   string name;
   string address;
+  string query;
+  person found;
 
   while(1){
     cout << "住所録に登録する名前を入力してください(終了するには quit と入力してください):";
@@ -61,9 +73,9 @@ int main()
   abook.listFriends();
 
   found = abook.find(query);
-  if(found.name ~= ""){
-    cout << found.name << ":" << found.address;
-  }else{
-    cout << "見つかりませんでした。";
-  }
+  //if(found.name ~= ""){
+    //cout << found.name << ":" << found.address;
+  //}else{
+    //cout << "見つかりませんでした。";
+  //}
 }
