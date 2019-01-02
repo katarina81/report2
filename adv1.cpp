c41eedb50cbf371c07f75f6dbb26079768bab359
katarina81 <s1611476@u.tsukuba.ac.jp>
2019-01-02 20:45:58 +0900

    ex

--- adv1.cpp
+++ adv1.cpp
@@ -24,25 +24,30 @@ public:
   transTicket(): ticket(){};
 };
 
-class trainTicket : public transTicket {
-public:
-  trainTicket(): transTicket(){};
-};
-
 class hm{
 public:
   int hour;
   int minute;
+};
 
-}
+class trainTicket : public transTicket {
+public:
+  trainTicket(): transTicket(){};
+  hm departureTime;
+  hm arrivalTime;
+};
+
+
 
 int main()
 {
   int ticketNum;
+  int hour;
+  int minute;
   cout << "チケットを何枚購入しますか? ";
   cin >> ticketNum;
   cout << "\n";
-  transTicket* tickets = new transTicket [ticketNum];
+  trainTicket* tickets = new trainTicket [ticketNum];
   for(int i = 0; i < ticketNum; i++){
     tickets[i].id = i+1;
     cout << i+1 << "人目の利用者の名前を入力してください: ";
@@ -51,13 +56,19 @@ int main()
     cin >> tickets[i].origin;
     cout << " 到着地を入力してください: ";
     cin >> tickets[i].destination;
-    vout << "出発時間を入力してください: ";
+    cout << " 出発時間を入力してください: ";
     scanf("%d:%d", &hour, &minute);
+    tickets[i].departureTime.hour = hour;
+    tickets[i].departureTime.minute = minute;
+    cout << " 到着時間を入力してください: ";
+    scanf("%d:%d", &hour, &minute);
+    tickets[i].arrivalTime.hour = hour;
+    tickets[i].arrivalTime.minute = minute;
   }
   cout << "\n チケット利用者一覧:\n";
   for(int i = 0; i < ticketNum; i++){
-    transTicket t = tickets[i];
-    cout << " " << t.id << " : " << t.user->name << " : " << t.origin << " => " << t.destination << "\n";
+    trainTicket t = tickets[i];
+    cout << " " << t.id << " : " << t.user->name << " : " << t.origin << "(" << t.departureTime.hour << ":" << t.departureTime.minute << ") => " << t.destination << "(" << t.arrivalTime.hour << ":" << t.arrivalTime.minute << ")\n";
   }
   cout << "\n";
 }
